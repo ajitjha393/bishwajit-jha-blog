8e8b160cbe2b0506c1c264c59351ffb175efdcf2
Bishwajit jha <ajitjha393@gmail.com>
2020-05-29 23:30:10 +0530

    Updated Blog Index page and name

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -1,5 +1,5 @@
 ---
-title: Hello JS!
+title: Hello JS Guys!
 date: "2015-05-01T22:12:03.284Z"
 description: "Hello World"
 ---
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `DevLine`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Bishwajit Jha`,
+      summary: `who lives in Mumbai building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://bishwajit-jha.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `BISHWAJITjha6`,
     },
   },
   plugins: [
